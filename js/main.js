c165f3dab95e78dced59fcf279e7233d0dddf571
emetdas <emet@b2bleadstaff.com>
2021-04-25 18:49:43 +0600

    javascript coding is running

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
+let navIcon = document.querySelector('.mobile-manue');
+
+
+
+
+// mobile naviation-start
+navIcon.addEventListener('click',()=>{
+    navIcon.classList.toggle('active');
+});
+// mobile naviation-end
+
+
+
 
 
