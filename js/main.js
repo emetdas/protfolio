42ed358b95fd9e41f95281a94bd7497209720434
emetdas <emet@b2bleadstaff.com>
2021-04-26 11:49:45 +0600

    one update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
 let navIcon = document.querySelector('.mobile-manue');
-
-
-
-
+let homeSection = document.querySelector('.home');
+let header = document.querySelector('.header');
+let nav = document.querySelector('nav');
 // mobile naviation-start
 navIcon.addEventListener('click',()=>{
     navIcon.classList.toggle('active');
 });
 // mobile naviation-end
+// sticky navigation-start
+let  observer = (entris)=>{
+    const [entry] = entris;
+    if (!entry.isIntersecting) {
+        header.classList.add('sticky');
+    }
+    else{
+        header.classList.remove('sticky');  
+    }
+}
+
+const Headerobserve = new IntersectionObserver(observer,{
+    root:null,
+    threshold:0,
+    rootMargin:`-${100}px`
+});
+Headerobserve.observe(homeSection);
+// sticky navigation-end
 
 
 
