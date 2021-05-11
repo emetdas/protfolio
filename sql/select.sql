8ff3261b1a8593304a4d76ebb9e08c9d09f2c54f
emetdas <emet@b2bleadstaff.com>
2021-05-11 23:38:11 +0600

    phone number

--- sql/select.sql
+++ sql/select.sql
@@ -7,7 +7,7 @@
  CREATE TABLE IF NOT EXISTS `contactForm` (
    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
    `name` VARCHAR(255),
-   PRIMARY KEY `pk_id`(`id`)
+   `phone`
  ) ENGINE = InnoDB;
  
 
