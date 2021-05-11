5fc67227236b0b979f7b79e2575c275bf3b3c9c6
emetdas <emet@b2bleadstaff.com>
2021-05-11 23:37:28 +0600

    contact form

--- sql/select.sql
+++ sql/select.sql
@@ -4,7 +4,7 @@
  
  2:CREATE TABLE:---
  
- CREATE TABLE IF NOT EXISTS `` (
+ CREATE TABLE IF NOT EXISTS `contactForm` (
    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
    `name` VARCHAR(255),
    PRIMARY KEY `pk_id`(`id`)
