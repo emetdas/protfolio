32d50737fe6283e935429919a71429ac18b6bec8
emetdas <emet@b2bleadstaff.com>
2021-05-11 23:35:20 +0600

    update

--- sql/select.sql
+++ sql/select.sql
@@ -2,6 +2,12 @@
 
  CREATE DATABASE cods;
  
-
+ 2:CREATE TABLE:---
+ 
+ CREATE TABLE IF NOT EXISTS `table_name` (
+   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+   `name` VARCHAR(255),
+   PRIMARY KEY `pk_id`(`id`)
+ ) ENGINE = InnoDB;
  
 
